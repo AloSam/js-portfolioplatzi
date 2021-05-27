e3be091016b06af73fcb031debb452dc4fdfc32a
Fabricio Samanez <fabricio.samanez@gmail.com>
2021-05-27 12:00:47 -0500

    imagenes como hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
@@ -28,6 +29,10 @@ module.exports = {
             {
                 test: /\.css|.styl$/i,
                 use: [ MiniCssExtractPlugin.loader, 'css-loader' , 'stylus-loader' ]
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
             }
         ]
     },
@@ -39,5 +44,13 @@ module.exports = {
             filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
         }),
         new MiniCssExtractPlugin(), // INSTANCIAMOS EL PLUGIN
+        new CopyPlugin({
+            patterns:[
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
     ]
 }
